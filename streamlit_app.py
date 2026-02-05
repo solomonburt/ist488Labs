a32424bcf37a331349a9b0b55e3beed235ec827b
solomonburt <sdburt@syr.edu>
2026-02-05 14:54:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Define the pages
 lab1 = st.Page("Lab1.py", title="Lab 1")
-lab2 = st.Page("Lab2.py", title="Lab 2", default=True)
+lab2 = st.Page("Lab2.py", title="Lab 2")
 lab3 = st.Page("Lab3.py", title="Lab 3", default=True)
 # Create navigation
 pg = st.navigation([lab1, lab2, lab3])
