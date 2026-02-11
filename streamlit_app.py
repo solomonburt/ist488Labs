3601c9d0ab34f5f560922291bbe3bead4c949d1f
solomonburt <sdburt@syr.edu>
2026-02-11 13:35:09 -0500

    Add Lab 4 page to the Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit as st
 # Define the pages
 lab1 = st.Page("Lab1.py", title="Lab 1")
 lab2 = st.Page("Lab2.py", title="Lab 2")
-lab3 = st.Page("Lab3.py", title="Lab 3", default=True)
+lab3 = st.Page("Lab3.py", title="Lab 3")
+lab4 = st.Page("Lab4.py", title="Lab 4", default=True)
 # Create navigation
-pg = st.navigation([lab1, lab2, lab3])
+pg = st.navigation([lab1, lab2, lab3, lab4])
 pg.run()
 
